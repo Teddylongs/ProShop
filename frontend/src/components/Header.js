635a8bdeedbb26b0af3675fa0cf5280560f01820
Teddylongs <eifemade@gmail.com>
2020-12-15 14:17:13 -0500

    update

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,29 @@
 import React from "react";
-import { Container, Row, Col, Navbar, Nav} from "react-bootstrap";
+import {LinkContainer} from 'react-router-bootstrap'
+import { Container, Row, Col, Navbar, Nav } from "react-bootstrap";
 
 const Header = () => {
   return (
     <header>
-     <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
-  <Navbar.Brand href="#home">ProShop</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-    </Nav>
-    
-  </Navbar.Collapse>
-</Navbar>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <LinkContainer to='/'>
+        <Navbar.Brand>ProShop</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+          <LinkContainer to='/cart'>
+            <Nav.Link >
+              <i className="fas fa-shopping-cart"></i>Cart
+            </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/login'>
+            <Nav.Link >Sign In</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+        
+      </Navbar>
     </header>
   );
 };
